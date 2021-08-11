4eba53edf8af11197028574004c5d57dfc4c2116
Aliya Nigamova <aliya.nigamova@cern.ch>
2021-08-11 15:10:49 +0200

    update the reweighting script

--- scripts/run_reweighting.py
+++ scripts/run_reweighting.py
@@ -103,18 +103,18 @@ if __name__ == "__main__":
     cern = '/eos/cms/store/mc/RunIIAutumn18NanoAODv6/ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8/'
 
     input_files = [
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/17227660-5AAA-7844-A6B9-B38362ABE23F.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/38D746F3-783A-064E-9AC3-AB2D85032250.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/5CE71A7A-B111-DE40-907A-7ABCCDC6315C.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/688060C7-151C-4944-82F2-5E691AED6AC9.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/99F692D0-AB49-A840-9D79-F8A062D1441C.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/DFD2630A-096A-DB4F-932C-C97497CA6C2F.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/230000/3F17120C-D93E-DB47-81E9-37F3080590CE.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/230000/C810B8DD-6B80-C444-96B9-43237A8219B2.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/240000/4D79ED9F-BA4A-7F4D-AEA9-3DC55901686A.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/240000/89DF89EF-660B-1045-8C44-80274AF0B4EA.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/240000/D40F0261-714C-5540-BE83-D9AEC9A02AC3.root',
-    desy+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/20000/0694B929-669C-314B-A92B-1F133051723B.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/17227660-5AAA-7844-A6B9-B38362ABE23F.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/38D746F3-783A-064E-9AC3-AB2D85032250.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/5CE71A7A-B111-DE40-907A-7ABCCDC6315C.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/688060C7-151C-4944-82F2-5E691AED6AC9.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/99F692D0-AB49-A840-9D79-F8A062D1441C.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/230000/DFD2630A-096A-DB4F-932C-C97497CA6C2F.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/230000/3F17120C-D93E-DB47-81E9-37F3080590CE.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/230000/C810B8DD-6B80-C444-96B9-43237A8219B2.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/240000/4D79ED9F-BA4A-7F4D-AEA9-3DC55901686A.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/240000/89DF89EF-660B-1045-8C44-80274AF0B4EA.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/240000/D40F0261-714C-5540-BE83-D9AEC9A02AC3.root',
+    cern+'NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/20000/0694B929-669C-314B-A92B-1F133051723B.root',
     ]
 
     rw_path = args[1]
@@ -124,8 +124,8 @@ if __name__ == "__main__":
         options.branchsel_out = options.branchsel
     if options.method=="vhbb":
         #modules = [rw.VHbbReweighter(rw_path, verb=options.verb)]
-        modules = [vhbb.vhbb2018_gen(),rw.VHbbReweighter(rw_path, verb=options.verb)]
-        #modules = [vhbb.jmeCorrections2018MC(),vhbb.jmeCorrections2018MCAll(),vhbb.mhtVHbb(),vhbb.vhbb2018(), rw.VHbbReweighter(rw_path, verb=options.verb)]
+        #modules = [vhbb.vhbb2018_gen(),rw.VHbbReweighter(rw_path, verb=options.verb)]
+        modules = [vhbb.jmeCorrections2018MC(),vhbb.jmeCorrections2018MCAll(),vhbb.jmeCorrections2018AK8MC(),vhbb.jmeCorrections2018AK8MCAll(),vhbb.mhtVHbb(),vhbb.vhbb2018(), rw.VHbbReweighter(rw_path, verb=options.verb)]
         #modules = [puWeight_2018(),jmeCorrections2018MC(),jmeCorrections2018MCAll(),mhtVHbb(),vhbb.vhbb2018(), rw.VHbbReweighter(rw_path, verb=options.verb)]
     else:
         Reweighter = getattr(rw, options.method+"Reweighter")
